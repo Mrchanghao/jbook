2d264347beedb572e46f0805bd3d733b15eb2e88
mrchanghao <lnl2048@gmail.com>
2021-03-30 18:52:39 +0900

    플러그인 및 린트 업데이트

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100, // 화면 폭
+  tabWidth: 2, // 탭간격, 스페이스 설정시 2칸 띄어쓰기
+  singleQuote: true, // ' 사용
+  trailingComma: 'all',
+  bracketSpacing: true,
+  semi: true,
+  useTabs: false,
+  arrowParens: 'avoid',
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,52 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState, useEffect, useRef } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from './plugin/unpkg-path.plugin';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const clickHandler = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+    });
+
+    // console.log(result);
+
+    setCode(result.outputFiles[0].text);
+  };
+  return (
+    <div>
+      <textarea value={input} onChange={e => setInput(e.target.value)} />
+      <div>
+        <button onClick={clickHandler}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// npm view react dist.tarball --> 설치된 패키지 파일들을 보내주는 링크
+// package들을 어떻게 번들링으로 만드는가...
+// unpkg를 사용 하여 퍼블릭 cdn으로 만든다
